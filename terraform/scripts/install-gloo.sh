adf96407d5b44cc703e7883bcf49409e087058b4
Jonathan E. Adiaheno <jonathan@adiaheno.com>
2024-08-17 21:23:38 -0400

    fix: :bug: yes to plugin install

--- terraform/scripts/install-gloo.sh
+++ terraform/scripts/install-gloo.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 
 DIR="$( cd "$( dirname "$0" )" && pwd )"
-
+yes Y | gcloud components install kubectl
 helm repo add gloo https://storage.googleapis.com/solo-public-helm
 helm repo update
+
 
 helm install gloo gloo/gloo \
   --create-namespace \
