c3c16a47b76bc375eac3b62809fa9dd4ee73f704
Jonathan E. Adiaheno <jonathan@adiaheno.com>
2024-08-17 22:46:11 -0400

    feat: :sparkles: removing anything gloo

--- terraform/load-balancer.tf
+++ terraform/load-balancer.tf
@@ -7,8 +7,8 @@
 # More information is available here: https://cloud.google.com/load-balancing/docs/https/proxy-only-subnets
 resource "google_compute_subnetwork" "proxy" {
   depends_on = [google_compute_network.default]
-  provider = google-beta
-  name          = "proxy-only-subnet"
+  provider   = google-beta
+  name       = "proxy-only-subnet"
   # This CIDR doesn't conflict with GKE's subnet
   ip_cidr_range = "11.129.0.0/23"
   project       = google_compute_network.default.project
@@ -21,22 +21,21 @@ resource "google_compute_subnetwork" "proxy" {
 # https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/compute_region_backend_service
 resource "google_compute_region_backend_service" "default" {
   # This cannot be deployed until the ingress gateway is deployed and the standalone NEG is automatically created
-  depends_on = [null_resource.gloo, null_resource.delete_ingressgateway]
-  project = google_compute_subnetwork.default.project
-  region  = google_compute_subnetwork.default.region
+  project     = google_compute_subnetwork.default.project
+  region      = google_compute_subnetwork.default.region
   name        = "l7-xlb-backend-service-http"
   protocol    = "HTTP"
   timeout_sec = 10
 
   # Scheme required for a Regional External HTTP Load Balancer. This uses an external managed Envoy proxy
   load_balancing_scheme = "EXTERNAL_MANAGED"
-  health_checks = [google_compute_region_health_check.default.id]
+  health_checks         = [google_compute_region_health_check.default.id]
 
   backend {
     # See the gloo.tf for more information on the ingressgateway standalone NEG that is automatically created
-    group = "https://www.googleapis.com/compute/v1/projects/${var.project_id}/zones/${var.zone}/networkEndpointGroups/ingressgateway"
+    group           = "https://www.googleapis.com/compute/v1/projects/${var.project_id}/zones/${var.zone}/networkEndpointGroups/ingressgateway"
     capacity_scaler = 1
-    balancing_mode = "RATE"
+    balancing_mode  = "RATE"
     # This is a reasonable max rate for an Envoy proxy
     max_rate_per_endpoint = 3500
   }
@@ -57,23 +56,17 @@ resource "google_compute_region_backend_service" "default" {
   }
 }
 
-resource "null_resource" "delete_ingressgateway" {
-  provisioner "local-exec" {
-    when    = destroy
-    # Delete ingressgateway on destroy
-    command = "gcloud compute network-endpoint-groups delete ingressgateway --quiet"
-  }
-}
+
 
 # https://registry.terraform.io/providers/hashicorp/google-beta/latest/docs/resources/compute_region_health_check
 resource "google_compute_region_health_check" "default" {
   depends_on = [google_compute_firewall.default]
-  project = google_compute_subnetwork.default.project
-  region  = google_compute_subnetwork.default.region
-  name   = "l7-xlb-basic-check-http"
+  project    = google_compute_subnetwork.default.project
+  region     = google_compute_subnetwork.default.region
+  name       = "l7-xlb-basic-check-http"
   http_health_check {
     port_specification = "USE_SERVING_PORT"
-    request_path = "/"
+    request_path       = "/"
   }
   timeout_sec         = 1
   check_interval_sec  = 3
@@ -82,7 +75,7 @@ resource "google_compute_region_health_check" "default" {
 }
 
 resource "google_compute_address" "default" {
-  name = var.ip_address_name
+  name    = var.ip_address_name
   project = google_compute_subnetwork.default.project
   region  = google_compute_subnetwork.default.region
   # Required to be STANDARD for use with REGIONAL_MANAGED_PROXY
@@ -90,7 +83,7 @@ resource "google_compute_address" "default" {
 }
 
 resource "google_compute_firewall" "default" {
-  name = "fw-allow-health-check-and-proxy"
+  name    = "fw-allow-health-check-and-proxy"
   network = google_compute_network.default.id
   project = google_compute_network.default.project
   # Allow for ingress from the health checks and the managed Envoy proxy. For more information, see:
@@ -100,5 +93,5 @@ resource "google_compute_firewall" "default" {
     protocol = "tcp"
   }
   target_tags = ["${var.gke_cluster_name}"]
-  direction = "INGRESS"
+  direction   = "INGRESS"
 }
